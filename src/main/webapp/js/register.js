2b5649afb997389ed27494df3f42180626d77924
taotao <69858468+taotao047@users.noreply.github.com>
2021-11-02 14:08:03 +0800

    删除layui

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -1,57 +1,50 @@
-/*对输入框的修饰*/
-$(document).ready(function () {
-    $(this).delegate("input[placeholder]",'focus blur',function () {
-        let elem = $(this);
-        if (elem.is(":focus")){
-            elem.css("border","1px dot #4285f4");
-        }
-    })
-})
-
-
+function submit() {
+    console.log($("button[type=submit]"));
+    $("button[type=submit]").after()
+}
 
 //注册提交
-function submit() {
-    /*对一些行为的处理*/
-    var registerInfo = document.getElementById("registerInfo");
-    if (this.uID.value == "" || this.uPassword.value == "" || this.uEmail.value == "" || this.uPhone.value == "") {
-        registerInfo.style.setProperty("visibility", "visible");
-        registerInfo.innerHTML = "请完善您的注册信息！";
-        return;
-    }
-    if (!document.getElementById("agreeBox").checked) {//没有勾选同意协议
-        registerInfo.innerHTML = "请阅读并同意下列协议";
-        registerInfo.style.setProperty("visibility", "visible");
-        return;
-    }
-    registerInfo.style.setProperty("visibility", "hidden");
-    registerInfo.innerHTML = "";
-    var uIDV = this.uID.value;
-    var uPasswordV = this.uPassword.value;
-    var uEmailV = this.uEmail.value;
-    var uPhoneV = this.uPhone.value;
-    /*执行注册事务*/
-    var xhr = new XMLHttpRequest();
-    xhr.open("post", "http://localhost:8080/register");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    var sendText="uID=" + uIDV + "&" +
-        "uPassword=" + uPasswordV + "&" + "uEmail=" + uEmailV + "&" + "uPhone=" +
-        uPhoneV;
-    var encodeText = encodeURI(encodeURI(sendText));
-    console.log(encodeText);
-    xhr.send(encodeText);
-    xhr.onload = function () {
-        var respText = xhr.responseText;
-        console.log(respText);
-        if (respText.match("true")) {
-            localStorage.setItem("uID", uIDV);
-            localStorage.setItem("uPassword", uPasswordV);
-            window.open("../yewen.html");
-        } else {
-            console.log("注册失败");
-            registerInfo.innerHTML = "手机号、邮箱、用户名重复！"
-            registerInfo.style.setProperty("visibility", "visible");
-        }
-    }
-
-}
+// function submit() {
+//     /*对一些行为的处理*/
+//     var registerInfo = document.getElementById("registerInfo");
+//     if (this.uID.value == "" || this.uPassword.value == "" || this.uEmail.value == "" || this.uPhone.value == "") {
+//         registerInfo.style.setProperty("visibility", "visible");
+//         registerInfo.innerHTML = "请完善您的注册信息！";
+//         return;
+//     }
+//     if (!document.getElementById("agreeBox").checked) {//没有勾选同意协议
+//         registerInfo.innerHTML = "请阅读并同意下列协议";
+//         registerInfo.style.setProperty("visibility", "visible");
+//         return;
+//     }
+//     registerInfo.style.setProperty("visibility", "hidden");
+//     registerInfo.innerHTML = "";
+//     var uIDV = this.uID.value;
+//     var uPasswordV = this.uPassword.value;
+//     var uEmailV = this.uEmail.value;
+//     var uPhoneV = this.uPhone.value;
+//     /*执行注册事务*/
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("post", "http://localhost:8080/register");
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//     var sendText="uID=" + uIDV + "&" +
+//         "uPassword=" + uPasswordV + "&" + "uEmail=" + uEmailV + "&" + "uPhone=" +
+//         uPhoneV;
+//     var encodeText = encodeURI(encodeURI(sendText));
+//     console.log(encodeText);
+//     xhr.send(encodeText);
+//     xhr.onload = function () {
+//         var respText = xhr.responseText;
+//         console.log(respText);
+//         if (respText.match("true")) {
+//             localStorage.setItem("uID", uIDV);
+//             localStorage.setItem("uPassword", uPasswordV);
+//             window.open("../yewen.html");
+//         } else {
+//             console.log("注册失败");
+//             registerInfo.innerHTML = "手机号、邮箱、用户名重复！"
+//             registerInfo.style.setProperty("visibility", "visible");
+//         }
+//     }
+//
+// }
